2090f2e234dc87a2df21a9f606086f7693d92f95
abhijeet1997 <jaganmohan19972016@gmail.com>
2017-10-06 19:16:01 +0530

    Firebase implemented with user authentication

---
+++ app/src/main/java/m/chatapp/UserDetails.java
@@ -0,0 +1,12 @@
+package m.chatapp;
+
+/**
+ * Created by kjaganmohan on 06/10/17.
+ */
+
+public class UserDetails {
+
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
